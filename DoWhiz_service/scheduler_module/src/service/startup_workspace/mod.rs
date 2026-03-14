9c5a33e1b3fd81512efacaa297490e376bd06f50
Logan <logan.yegaoyang@gmail.com>
2026-03-13 18:33:29 -1000

    Add startup workspace home snapshot seam

--- DoWhiz_service/scheduler_module/src/service/startup_workspace/mod.rs
+++ DoWhiz_service/scheduler_module/src/service/startup_workspace/mod.rs
@@ -2,6 +2,7 @@ pub mod bootstrap;
 pub mod intake;
 pub mod provisioning;
 pub mod resource_mapping;
+pub mod workspace_home;
 
 pub use bootstrap::{bootstrap_workspace_plan, StartupWorkspaceBootstrapPlan};
 pub use intake::normalize_and_validate_blueprint;
@@ -9,3 +10,6 @@ pub use provisioning::{
     derive_provisioning_snapshot, ProvisioningStepStatus, StartupProvisioningSnapshot,
 };
 pub use resource_mapping::build_starter_resource_plan;
+pub use workspace_home::{
+    bootstrap_workspace_home_snapshot, build_workspace_home_snapshot, WorkspaceHomeSnapshot,
+};
